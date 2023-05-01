04978626aa3f0ad35ddb9e9e84ea1fa8a597fc3e
Mikhail_Proshin <mikhail.proshin@ah.nl>
2023-05-01 23:05:13 +0400

    update script

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,11 @@ cd /Users/Mikhail_Proshin/work/shop-react-redux-cloudfront/
 rm -rf ./dist;
 npm run build;
 # clear data
-ssh 13.53.61.166;
-rm /var/www/html/*;
-rm -rf /home/ubuntu/nestjs-rest-api;
-exit;
+ssh ubuntu@13.53.61.166 'sudo rm -rf /var/www/html/*'
+ssh ubuntu@13.53.61.166 'sudo rm -rf /home/ubuntu/nestjs-rest-api'
 # transfer data
-scp /Users/Mikhail_Proshin/work/nestjs-rest-api ubuntu@13.53.61.166:/home/ubuntu;
-scp /Users/Mikhail_Proshin/work/shop-react-redux-cloudfront/dist ubuntu@13.53.61.166:/var/www/html
+scp -r /Users/Mikhail_Proshin/work/nestjs-rest-api ubuntu@13.53.61.166:/home/ubuntu;
+scp -r /Users/Mikhail_Proshin/work/shop-react-redux-cloudfront/dist ubuntu@13.53.61.166:/var/www/html
 # run new version
-ssh 13.53.61.166;
-cd /home/ubuntu/nestjs-rest-api;
-npm run buld;
-pm2 delete main
-pm2 start ./dist/main.js;
-systemctl restart nginx;
+ssh ubuntu@13.53.61.166 'cd /home/ubuntu/nestjs-rest-api;npm run buld;pm2 delete main';
+ssh ubuntu@13.53.61.166 'pm2 start ./nestjs-rest-api/dist/main.js; sudo systemctl restart nginx;'
